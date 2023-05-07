7a6ca276480b1224a5d1164cc29098717bf58f67
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-05-07 12:54:45 +0200

    Treni in data odierna

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Train;
+use Carbon\Carbon;
 
 class PageController extends Controller
 {
     public function index() {
 
-        $trains = Train::all();
+        // $trains = Train::all();
+        $trains = Train::whereDate('departure_time', Carbon::today())->get();
 
         return view('home', compact('trains'));
     }
